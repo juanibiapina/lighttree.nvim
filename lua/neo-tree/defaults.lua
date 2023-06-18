aa3a104a7391219a54e20afe3b34eaf45bff7850
Juan Ibiapina <juanibiapina@gmail.com>
2023-06-18 06:23:24 +0200

    Remove code

--- lua/neo-tree/defaults.lua
+++ lua/neo-tree/defaults.lua
@@ -15,7 +15,6 @@ local config = {
     max_lines = 10000, -- How many lines of git status results to process. Anything after this will be dropped.
                        -- Anything before this will be used. The last items to be processed are the untracked files.
   },
-  log_level = "info", -- "trace", "debug", "info", "warn", "error", "fatal"
   log_to_file = false, -- true, false, "/path/to/file.log", use :NeoTreeLogs to show the file
   popup_border_style = "NC", -- "double", "none", "rounded", "shadow", "single" or "solid"
   resize_timer_interval = 500, -- in ms, needed for containers to redraw right aligned and faded content
