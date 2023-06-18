bafaa646c9920f7e8c22fb44064f56d022e19a2d
Juan Ibiapina <juanibiapina@gmail.com>
2023-06-18 06:59:20 +0200

    Remove code

--- lua/neo-tree.lua
+++ lua/neo-tree.lua
@@ -1,11 +1,6 @@
-local log = require("neo-tree.log")
 local setup = require("neo-tree.setup")
 
 local M = {}
-
-M.set_log_level = function(level)
-  log.set_level(level)
-end
 
 M.setup = function(config)
   M.config = require("neo-tree.setup").merge_config(config)
