7faf3ba7ad4f5f5e75f69c3a0d4efb9c7d1dd072
Juan Ibiapina <juanibiapina@gmail.com>
2023-07-24 13:07:00 +0200

    refactor: Stop using Nui deprecated option

--- lua/neo-tree/ui/renderer.lua
+++ lua/neo-tree/ui/renderer.lua
@@ -591,7 +591,7 @@ end
 create_tree = function(state)
   state.tree = NuiTree({
     ns_id = highlights.ns_id,
-    winid = state.winid,
+    bufnr = state.bufnr,
     get_node_id = function(node)
       return node.id
     end,
